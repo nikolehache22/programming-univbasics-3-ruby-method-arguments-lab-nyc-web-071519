8e7b90d96bbe67056ffa2104fafebeca3de92adc
Nikole Hache <nh131215@gmail.com>
2019-07-09 13:06:13 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,16 @@
+def introduction (name)
+  name =
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language (name, language)
+  name =
+  language =
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+def introduction_with_language_optional (name, language='Ruby')
+    name =
+    language =
+    puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
--- spec/introduction_spec.rb
+++ spec/introduction_spec.rb
@@ -1,6 +1,6 @@
-require "spec_helper"
+ require "spec_helper"
 
-describe "#introduction" do
+ describe "#introduction" do
   it "takes in an argument of a name and puts out a phrase with that name using string interpolation" do
     expect{introduction("Josh")}.to output("Hi, my name is Josh.\n").to_stdout
     expect{introduction("Dan")}.to output("Hi, my name is Dan.\n").to_stdout
