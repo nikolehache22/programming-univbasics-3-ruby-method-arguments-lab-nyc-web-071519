d24e515b0a1e841935a83395ed8146e2bb946a46
Nikole Hache <nh131215@gmail.com>
2019-07-09 12:59:31 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,12 +1,17 @@
 def introduction (name)
+  name =
   print "Hi, my name is #{name}."
 end
 
 def introduction_with_language (name, language)
+  name =
+  language =
   print "Hi, m name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language_optional (name, language)
+def introduction_with_language_optional (name, language='Ruby')
   do
+    name =
+    language =
     print "Hi, m name is #{name} and I am learning to program in #{language}."
 end
